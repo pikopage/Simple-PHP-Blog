216413a6b1940ebde8778d1d8829ec7cd267fcb3
pikopage <42669365+pikopage@users.noreply.github.com>
2020-04-14 13:15:47 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,11 +1,12 @@
-<div class="w3-container w3-teal">
-    <p>All rights reserved | <?php echo date("Y"); ?></p>
+<?PHP
+$footer = "<div class='w3-container w3-round $card_color[0]'>
+    <p class='w3-bar-item  w3-padding-large'>[PiKo] Page | ".date("Y")."<a href='index.php?login=yes'><i class='fa fa-lock w3-right'></i></a></p>
 </div>
-
-<script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.19.1/trumbowyg.min.js"></script>
+<script src='//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.19.1/trumbowyg.min.js'></script>
 <script>
     $('#description').trumbowyg();
 </script>
-
-</body>
+</div>
+</body>";
+?>
