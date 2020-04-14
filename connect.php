043b5215256a15dae24e4b6f943c7476d2781cfb
pikopage <42669365+pikopage@users.noreply.github.com>
2020-04-14 14:53:33 +0200

    Update connect.php
    security update

--- connect.php
+++ connect.php
@@ -1,4 +1,7 @@
 <?php
+if (realpath(__FILE__) == realpath($_SERVER['DOCUMENT_ROOT'].$_SERVER['SCRIPT_NAME'])) {
+echo "File can't be called separately, it is part of other script";
+}else{
 $dbhost 	= "localhost";
 $dbuser 	= "root";
 $dbpass 	= "";
@@ -11,3 +14,4 @@ if(mysqli_connect_errno())
 	print_f("Connection Failed: %s\n",mysqli_connect_error());
 }
 mysqli_set_charset($dbcon,$charset);
+}
