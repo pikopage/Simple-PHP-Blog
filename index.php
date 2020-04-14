e1ef94ff409f3e9c9291d8a32dcebefb802f681f
pikopage <42669365+pikopage@users.noreply.github.com>
2020-04-14 12:51:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,61 +1,65 @@
 <?php
-include("header.php");
-include("connect.php");
-?>
+session_start();
+/*Check presense of all used subfiles*/
+//require("actions.php");
+require("connect.php");
+require("header.php");
+require("menu.php");
+require("footer.php");
 
-<div class="w3-panel">
-    <p>This is a simple blog project for my PHP development skills.</p>
+/* First post - Pinned only and only first one is fetched from DB. --START */
+$sql = "SELECT description FROM posts where deleted = 0 and visible = 1 and pinned = 1 ORDER BY id DESC";
+$result = $dbcon->query($sql);
+$maindes = $result->fetch_row();
+$mainpost = "
+<div class='w3-card w3-round w3-padding-small w3-margin-bottom'>
+$maindes[0]
 </div>
-
-<?php
-// COUNT 
-$sql = "SELECT COUNT(*) FROM posts";
-$result = mysqli_query($dbcon, $sql);
-$r = mysqli_fetch_row($result);
-$numrows = $r[0];
-
-$rowsperpage = 5;
-$totalpages = ceil($numrows / $rowsperpage);
-
+";
+/* First post - Pinned only and only first one is fetched from DB. --END */
+/* Posts count - counting moved to DB ---- START */
+$sql = "SELECT a.value as value, ceil(count(*)/a.value) as totalpages FROM setting as a join posts as b where a.variable = 'rowsperpage' and b.deleted = 0 and b.visible = 1 and b.pinned = 0";
+$result = $dbcon->query($sql);
+$row = $result->fetch_assoc();
+$totalpages = $row['totalpages'];
+$rowsperpage = $row['value'];
 if (isset($_GET['page']) && is_numeric($_GET['page'])) {
     $page = (INT)$_GET['page'];
 }
-
 if ($page > $totalpages) {
     $page = $totalpages;
 }
-
 if ($page < 1) {
     $page = 1;
 }
 $offset = ($page - 1) * $rowsperpage;
-
-$sql = "SELECT * FROM posts ORDER BY id DESC LIMIT $offset, $rowsperpage";
-$result = mysqli_query($dbcon, $sql);
-
-if (mysqli_num_rows($result) < 1) {
-    echo '<div class="w3-panel w3-pale-red w3-card-2 w3-border w3-round">Nothing to display</div>';
+/* Posts count ---- END */
+/* Preview - Create visible posts previw - START */
+$sql = "SELECT id, title, description, DATE_FORMAT(date,'%D %M %Y') as date, char_length(description)-char_length(REPLACE(description, ' ', '')) as words FROM posts where deleted = 0 and visible = 1 AND pinned = 0 ORDER BY id DESC LIMIT $offset, $rowsperpage";
+$result = $dbcon->query($sql);
+$numrows = $result->num_rows;
+if ($numrows < 1) {
+    $posts = "<div class='w3-panel w3-pale-red w3-card-2 w3-border w3-round w3-margin-bottom'>Nothing to display. Write first posts please.</div>";
 }
-while ($row = mysqli_fetch_assoc($result)) {
-
+while ($row = $result->fetch_assoc()) {
     $id = htmlentities($row['id']);
     $title = htmlentities($row['title']);
-    $des = htmlentities($row['description']);
+    $des = $row['description'];
     $time = htmlentities($row['date']);
-
-    echo '<div class="w3-panel w3-sand w3-card-4">';
-    echo "<h3><a href='view.php?id=$id'>$title</a></h3><p>";
-
-    echo substr($des, 0, 100);
-
-    echo '</p><div class="w3-text-teal">';
-    echo "<a href='view.php?id=$id'>Read more...</a>";
-
-    echo '</div> <div class="w3-text-grey">';
-    echo "$time</div>";
-    echo '</div>';
+    $words = htmlentities($row['words']);
+$posts .= "<div id='post_$id'><div class='w3-card w3-round w3-padding-small w3-margin-bottom'>
+<h3><a href='#' onclick='return showpost($id)' style='w3-bar-item w3-round w3-button w3-padding-large w3-theme-d4'>$title</a></h3><p>
+".substr($des, 0, 100)."<a href='#' onclick='return showpost($id)'>... Read next $words words</a></p> <div class='w3-text-grey'>$time</div></div></div>";
 }
+/* Preview - END */
+/* Presenter - Show HTML Code - START */
+echo $head;
+echo $menu;
+echo $mainpost;
+echo $posts;
 
+/* Presenter - Show HTML Code - END */
+/* This code still waitiong for changes */
 
 echo "<div class='w3-bar w3-center'>";
 if ($page > 1) {
@@ -81,4 +85,4 @@ if ($page != $totalpages) {
 }
 echo "</div>";
 include("categories.php");
-include("footer.php");
+echo $footer;
