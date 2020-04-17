ef42c763ccc80913476b37a2402a6ba090d66f12
pikopage <42669365+pikopage@users.noreply.github.com>
2020-04-18 00:26:14 +0200

    Update getpost.php
    Edit func added

--- getpost.php
+++ getpost.php
@@ -4,6 +4,7 @@ ID of post is required in request*/
 session_start();
 require("connect.php");
 $id = (INT)$_GET['q'];
+$edit = (INT)$_GET['e'];
 
 $hsql = "UPDATE posts SET hits = hits +1 WHERE id = $id";
 $r = mysqli_query($dbcon, $hsql);
@@ -17,13 +18,31 @@ $des = $row['description'];
 $by = $row['posted_by'];
 $hits = $row['hits'];
 $time = $row['date'];
+/*show normal post or edit window in case of request*/
+if ($edit == 1 and isset($_SESSION['username'])){
+    $posts .= "<form action='index.php?id=$id&action=2' method='POST' class='w3-container w3-round w3-green'>
+    <input type='hidden' name='id' value='$id'>
+    <label>Title</label>
+    <input type='text' class='w3-input w3-round w3-border' name='title' value='$title'>
 
-echo "<div class='w3-card w3-round w3-sand w3-padding-small w3-margin-bottom'>";
-echo "<h3>$title</h3>";
-echo '<div>';
-echo "$des<br>";
-echo '<div class="w3-text-grey">';
-echo "The article was posted by $by on $time<br>";
-echo "It was read $hits times.<br></div></div>";
+    <label>Description</label>
+    <textarea class='w3-input w3-round w3-border' id='description' name='description'>$des</textarea>
 
+    <button class='w3-round w3-btn w3-margin ".$bck_color."' name='upd'><i class='fa fa-save w3-margin-right'></i>Save</button>
+    <a href='index.php' class='w3-round w3-btn w3-margin ".$bck_color."'><i class='fa fa-close w3-margin-right'></i>Cancel</a>
+    </form>";
+}else{
+$posts .= "<div class='w3-card w3-round w3-sand w3-padding-small w3-margin-bottom'><h3>$title</h3>
+<div>$des<br>
+<div class='w3-text-grey'>
+The article was posted by $by on $time<br>
+It was read $hits times.<br>";
+/* Show edit and delete option in case of user*/
+if (isset($_SESSION['username'])) {
+$posts .= "<a class='w3-bar-item w3-round w3-btn w3-margin-right w3-margin-bottom ".$bck_color." w3-text-green' href='#' onclick='return editpost($id)'><i class='fa fa-edit w3-margin-right'></i>Edit</a>
+    <a class='w3-bar-item w3-round w3-btn w3-margin-right w3-margin-bottom ".$bck_color." w3-text-red' href='index.php?id=$id&action=1'><i class='fa fa-close w3-margin-right'></i>Delete</a>";
+}
+$posts .= "</div></div>";
+}
+echo $posts;
 ?>
