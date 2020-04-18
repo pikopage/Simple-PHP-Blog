aa4cfb91e23301e21e4dcf0fbb45a57478f2f16c
pikopage <42669365+pikopage@users.noreply.github.com>
2020-04-18 09:56:19 +0200

    Update menu.php
    New post func added.

--- menu.php
+++ menu.php
@@ -1,11 +1,11 @@
     <?php
-    /* Menu also manage user logon It is only included in index.php, does not work separetly */
-if (realpath(__FILE__) == realpath($_SERVER['DOCUMENT_ROOT'].$_SERVER['SCRIPT_NAME'])) {
-echo "File can't be called separately, it is part of other script";
-}else{    
-if ($_GET['login']=="no") {
+    /* Menu also manage user logon. It is only included in index.php, does not work separetly */
+    if (realpath(__FILE__) == realpath($_SERVER['DOCUMENT_ROOT'].$_SERVER['SCRIPT_NAME'])) {
+        echo "File can't be called separately, it is part of other script";
+        }else{
+    if ($_GET['login']=="no") {
         session_destroy();
-     $menu .=  "<div class='w3-card w3-round ".$card_color." w3-margin-right w3-margin-bottom' style='max-width:570px'><div class='w3-bar-item'>User logged out.</div></div>";
+        $menu .=  "<div class='w3-card w3-round ".$card_color." w3-margin-right w3-margin-bottom' style='max-width:570px'><div class='w3-bar-item'>User logged out.</div></div>";
     }
     if ($_GET['login']=="yes") {
          if (isset($_POST['log'])) {
@@ -33,13 +33,13 @@ if ($_GET['login']=="no") {
      }
      if ($_SESSION['username'] && $_GET['login']!=="no") {
         $user = $_SESSION['username'];
-        $menu = "<a href='new.php' class='w3-bar-item w3-round w3-btn w3-margin-right w3-margin-bottom ".$bck_color[0]."'> <i class='fa fa-file w3-margin-right'></i>New Post</a>";
+        $menu = "<a href='index.php?action=3' class='w3-bar-item w3-round w3-btn w3-margin-right w3-margin-bottom ".$bck_color."'> <i class='fa fa-file w3-margin-right'></i>New Post</a>";
     }
         if ($_SESSION['username'] && $_GET['login']!=="no") {
-        $menu .= "<a href='cat.php' class='w3-bar-item w3-round w3-btn w3-margin-right w3-margin-bottom ".$bck_color[0]."'><i class='fa fa-list-alt w3-margin-right'></i>Categories</a>";
+        $menu .= "<a href='cat.php' class='w3-bar-item w3-round w3-btn w3-margin-right w3-margin-bottom ".$bck_color."'><i class='fa fa-list-alt w3-margin-right'></i>Categories</a>";
     }
     if ($_SESSION['username'] && $_GET['login']!=="no") {
-        $menu .=  "<a href='index.php?login=no' class='w3-bar-item w3-round w3-btn w3-margin-right w3-margin-bottom ".$bck_color[0]."'><i class='fa fa-sign-out w3-margin-right'></i>Sign out</a>";
+        $menu .=  "<a href='index.php?login=no' class='w3-bar-item w3-round w3-btn w3-margin-right w3-margin-bottom ".$bck_color."'><i class='fa fa-sign-out w3-margin-right'></i>Sign out</a>";
     }
 }
     ?>
