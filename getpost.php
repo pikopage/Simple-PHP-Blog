2ef305db7cc1a2b434d36ca0fbabbc6a3f76120c
pikopage <42669365+pikopage@users.noreply.github.com>
2020-04-18 09:58:21 +0200

    Update getpost.php
    New post func added.

--- getpost.php
+++ getpost.php
@@ -8,16 +8,21 @@ $edit = (INT)$_GET['e'];
 
 $hsql = "UPDATE posts SET hits = hits +1 WHERE id = $id";
 $r = mysqli_query($dbcon, $hsql);
-
-$hsql = "SELECT id,title,description,posted_by, hits,DATE_FORMAT(date,'%D %M %Y') as date FROM posts WHERE id = '$id' AND deleted = 0 and visible = 1 AND pinned = 0";
+if (isset($_SESSION['username'])){
+    $visible = "";
+}else{
+    $visible = "and b.visible = 1";
+}
+$hsql = "SELECT id,title,description,posted_by, hits,DATE_FORMAT(date,'%D %M %Y') as date, visible FROM posts WHERE id = '$id' AND deleted = 0 $visible AND pinned = 0";
 $res = mysqli_query($dbcon, $hsql);
 $row = mysqli_fetch_assoc($res);
 $id = $row['id'];
 $title = $row['title'];
-$des = $row['description'];
+$description = $row['description'];
 $by = $row['posted_by'];
 $hits = $row['hits'];
 $time = $row['date'];
+$visible = $row['visible'];
 /*If it is requested, prepare edit window*/
 if ($edit == 1 and isset($_SESSION['username'])){
     $posts .= "<form action='index.php?id=$id&action=2' method='POST' class='w3-container w3-round w3-green'>
@@ -26,7 +31,7 @@ if ($edit == 1 and isset($_SESSION['username'])){
     <input type='text' class='w3-input w3-round w3-border' name='title' value='$title'>
 
     <label>Description</label>
-    <textarea class='w3-input w3-round w3-border' id='description' name='description'>$des</textarea>
+    <textarea class='w3-input w3-round w3-border' id='description' name='description' style='height: 400px;'>$description</textarea>
 
     <button class='w3-round w3-btn w3-margin ".$bck_color."' name='upd'><i class='fa fa-save w3-margin-right'></i>Save</button>
     <a href='index.php' class='w3-round w3-btn w3-margin ".$bck_color."'><i class='fa fa-close w3-margin-right'></i>Cancel</a>
@@ -34,14 +39,20 @@ if ($edit == 1 and isset($_SESSION['username'])){
 /*else only post for view*/
 }else{
 $posts .= "<div class='w3-card w3-round w3-sand w3-padding-small w3-margin-bottom'><h3>$title</h3>
-<div>$des<br>
+<div>$description<br>
 <div class='w3-text-grey'>
 The article was posted by $by on $time<br>
 It was read $hits times.<br>";
-/* Show edit and delete option, when user is logged on*/
+/* Show edit, visibility and delete option, when user is logged on*/
 if (isset($_SESSION['username'])) {
-$posts .= "<a class='w3-bar-item w3-round w3-btn w3-margin-right w3-margin-bottom ".$bck_color." w3-text-green' href='#post_$id' onclick='return editpost($id)'><i class='fa fa-edit w3-margin-right'></i>Edit</a>
-    <a class='w3-bar-item w3-round w3-btn w3-margin-right w3-margin-bottom ".$bck_color." w3-text-red' href='index.php?id=$id&action=1#post_$id'><i class='fa fa-close w3-margin-right'></i>Delete</a>";
+    if ($visible == 1){
+        $VisHTML = "w3-text-green' href='index.php?id=$id&action=4&vis=0#post_$id'><i class='fa fa-unlock w3-margin-right'></i>Visible";
+    }else{
+        $VisHTML = "w3-text-red' href='index.php?id=$id&action=4&vis=1#post_$id'><i class='fa fa-lock w3-margin-right'></i>Invisible";
+    }
+    $posts .= "<a class='w3-bar-item w3-round w3-btn w3-margin-right w3-margin-bottom ".$bck_color." w3-text-green' href='#post_$id' onclick='return editpost($id)'><i class='fa fa-edit w3-margin-right'></i>Edit</a>
+    <a class='w3-bar-item w3-round w3-btn w3-margin-right w3-margin-bottom ".$bck_color." w3-text-red' href='index.php?id=$id&action=1#post_$id'><i class='fa fa-close w3-margin-right'></i>Delete</a>
+    <a class='w3-bar-item w3-round w3-btn w3-margin-right w3-margin-bottom ".$bck_color." $VisHTML</a>";
 }
 $posts .= "</div></div>";
 }
